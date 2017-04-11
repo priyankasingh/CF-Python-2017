a6b8ccc34b4984c595a6741146ded7b9ccea7f15
Priyanka Singh <Rakhee@priyanka-mac.ecs.soton.ac.uk>
2017-04-11 10:36:49 +0100

    Updated files

--- Codes/twitter_api.py
+++ Codes/twitter_api.py
@@ -2,14 +2,14 @@
 
 import tweepy
 
-CONSUMER_KEY = "The key that you got tomorrow from your twitter account"
-CONSUMER_SECRET = "The consumer secret"
+CONSUMER_KEY = "fdsfsdgsdg" #"The key that you got tomorrow from your twitter account"
+CONSUMER_SECRET = "bdfbdfg"  # "The consumer secret"
 
-ACCESS_KEY = "Access key that we created from our respective accounts in the end"
-ACCESS_SECRET = "Access secret"
+ACCESS_KEY = "gfdgsg"   #"Access key that we created from our respective accounts in the end"
+ACCESS_SECRET = "fgsgsfg"   #"Access secret"
 
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 
 api=tweepy.API(auth)
-api.update_status('Test tweet!')
+api.update_status('Test tweet 2!')
